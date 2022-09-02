e02927540ecb7d56af18e0878727840a7b632083
dxwebster <adrianalimafm@gmail.com>
2022-09-01 22:28:22 -0300

    add result in #imc

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 function buildCalculateImc() {
   var heightElem = document.querySelector("#altura")
   var weightElem = document.querySelector("#peso")
-  console.log(heightElem)
-  console.log(weightElem)
 
   return function () {
     var height = heightElem.value
@@ -13,20 +11,18 @@ function buildCalculateImc() {
 }
 
 function doCalculateImc(height, weight) {
-  const Person = {
+  const payload = {
     height,
     weight,
-    imc: "",
-    imcDescription: "",
   }
 
-  const result = fetch("https://localhost:8080/calculate", {
+  const result = fetch("http://localhost:8080/calculate", {
     method: "POST",
-    body: JSON.stringify(Person),
+    body: JSON.stringify(payload),
     headers: { "Content-type": "application/json; charset=UTF-8" },
   }).then(response => response.json())
 
-  console.log("✅ ~ result", result)
+  document.querySelector("#imc").innerHTML = result
 }
 
 window.onload = function (evt) {
@@ -35,22 +31,3 @@ window.onload = function (evt) {
   var btn = document.querySelector("#main-action")
   btn.addEventListener("click", buildCalculateImc())
 }
-
-// function doCalculateImc(height, weight) {
-//     var imc = (weight / (height ** 2));
-//     document
-//         .querySelector('#imc')
-//         .innerHTML = translateImcToText(imc);
-// }
-
-// // Magreza, quando o resultado é menor que 18,5 kg/m2;
-// // Normal, quando o resultado está entre 18,5 e 24,9 kg/m2;
-// // Sobrepeso, quando o resultado está entre 24,9 e 30 kg/m2;
-// // Obesidade, quando o resultado é maior que 30 kg/m2;
-// function translateImcToText(imc) {
-//     if (imc < 18.5) return "Magreza";
-//     if (imc < 24.9) return "Normal";
-//     if (imc < 30) return "Sobrepeso";
-
-//     return "Obesidade";
-// }
